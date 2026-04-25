0cef12db3985875e877505a1f21e7af3f34cf09a
Chalani Amarasooriya <chalani.am810@gmail.com>
2026-04-25 15:24:39 +0530

    images fix

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: "export",
+  // REPLACE 'your-repo-name' with your actual GitHub repository name!
+  // It must start with a forward slash.
+  basePath: "/oudra-dss",
+
+  // This ensures CSS and JS files load correctly too
+  assetPrefix: "/oudra-dss/",
+
+  // Allows Next.js Image component to work with static exports
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
